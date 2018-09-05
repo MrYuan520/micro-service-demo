6952f6ababf2a0a4ac232dafadd1dab2dc93a7fb
YuanBo <544452023@qq.com>
2018-09-05 15:53:42 +0800

    latest 写错了！ ccc

--- user-edge-service/build.sh
+++ user-edge-service/build.sh
@@ -3,4 +3,4 @@
 mvn package
 
 docker build -t 192.168.5.155/micro-service-demo/user-edge-service:latest .
-docker push 192.168.5.155/micro-service-demo/user-edge-service:lastest
+docker push 192.168.5.155/micro-service-demo/user-edge-service:latest
